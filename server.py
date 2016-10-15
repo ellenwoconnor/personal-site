7515bbd3387284ca66d029eeb11ba8379e06cfbc
Ellen O'Connor <eoconnor9484@gmail.com>
2016-10-15 19:55:17 -0400

    First pass with index & research pages working

--- server.py
+++ server.py
@@ -1,24 +1,18 @@
 from flask import Flask, request, render_template
-from time import sleep
-from JobSearcher import JobSearcher
-
 app = Flask(__name__)
+import strings
 
 # app.secret_key = 'this-should-be-something-unguessable'
 
 @app.route('/')
 def index():
-
-    return render_template('index.html')
+    return render_template('index.html', strings=strings)
 
 @app.route('/research')
 def research():
-
     return render_template('research.html')
-
-@app.route('/professional')
-def professional():
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
