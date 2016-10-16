65934a9506d05c99f93de555d464485cb463fdac
Ellen O'Connor <eoconnor9484@gmail.com>
2016-10-15 22:33:21 -0400

    Add some styling, new pages

--- server.py
+++ server.py
@@ -1,16 +1,19 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
-import strings
 
 # app.secret_key = 'this-should-be-something-unguessable'
 
 @app.route('/')
 def index():
-    return render_template('index.html', strings=strings)
+    return render_template('index.html', strings=strings, current_page='about')
 
 @app.route('/research')
 def research():
-    return render_template('research.html')
+    return render_template('research.html', current_page='research')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html', current_page='resume')
 
 
 if __name__ == "__main__":
